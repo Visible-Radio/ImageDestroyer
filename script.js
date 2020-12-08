1d4a1adba65f2029aae3f3911cf5a7713496adb2
Patrick Kaipainen <patrick.kaipainen@gmail.com>
2020-12-07 22:21:04 -0500

    text handling in progress

--- script.js
+++ script.js
@@ -1,22 +1,9 @@
 // break image open
 // gridContainer.children[(parseInt(gridContainer.childElementCount/2))].style.setProperty(`margin-top`, 128 +`px`);
 
-//========================API NOTES==========================//
-
-// GET https://images-api.nasa.gov
-// GET /search?q={q}
-// &media_type=image specifies images only
-// &year_end=1991 specifies newest date
-// search?q=station specifies search string, in this case "station"
-
-//=============Availavle in the response object==============//
-// respObj.collection.items[0].links[0].href; // this is a thumbnail
-// respObj.collection.items[0].data[0].description;
-// respObj.collection.items[0].data[0].title;
-// respObj.collection.items[0].data[0].location;
-// respObj.collection.items[0].data[0].date_created;
-// respObj.collection.items[0].href;
-//===========================================================//
+// probably could improbe performance by resizing gridContainer through JS, incrementing by 12 px
+// instead of using the css transition on width
+// pixels are only going to overflow into a new row when that row has gotten 12 px wider..
 
 // (async () => {
 //======================GET THE SOURCE IMAGE=================//	
@@ -36,20 +23,28 @@ img.setAttribute('crossOrigin', '');
 		}
 	}
 
-	let srcIndex = 35;
+	let srcIndex = 17;
 	async function handleRender() {
-		let respObj;	
-		await getImageCollection().then(response => response.json()).then(response => respObj = response.collection.items);
-		console.log(respObj.length);
-		console.log(srcIndex);
-		console.log(respObj[srcIndex].links[0].href);
+		let respObj;
+		let description;
+		await getImageCollection()
+			.then(response => response.json())
+			.then(response => respObj = response.collection.items);
+		console.log(respObj);
+		console.log("description:", respObj[srcIndex].data[0].description);
+		console.log("description length:", respObj[srcIndex].data[0].description.length);	
+		console.log("returned items: ", respObj.length);
+		console.log("srcIndex: ", srcIndex);		
+
 		img.src = `${respObj[srcIndex].links[0].href}` + '?' + new Date().getTime();
+		imgDescription = respObj[srcIndex].data[0].description;
+
 		document.querySelector('.gridContainer').style.setProperty('transition-property', 'none');
 		document.documentElement.style.setProperty(
 		`--gridContainerWidthLeft`, 0 + 'px');
 
 		destroyDisplay();
-		Main();
+		Main(imgDescription);
 		srcIndex = respObj.length-1 > srcIndex ? srcIndex + 1 : 0;
 	}
 
@@ -60,7 +55,7 @@ img.setAttribute('crossOrigin', '');
 
 
 
-function Main() {
+function Main(imgDescription) {
 // ======================== Main Program ====================//
 img.onload = function() {
 	// get the canvas context
@@ -83,10 +78,16 @@ img.onload = function() {
 
   // for each pixel create a div
   const gridContainer = document.querySelector('.gridContainer');
+
+  // color the divs based on the values from the image
   const divDisplay = createDisplay(destroyedImg.length, gridContainer);
   console.log(gridContainer.childElementCount);
 
-	// color the divs based on the values from the image
+//********************************************************************
+  
+  sayHello(divDisplay, imgDescription.toUpperCase());
+//********************************************************************
+	
 	document.querySelector('.gridContainer').style.setProperty('transition-property', 'width');	
 	displayDraw1(divDisplay, destroyedImg);
 
@@ -102,6 +103,71 @@ img.onload = function() {
  };
 
  // =====================End Main Program ====================//
+}
+
+
+const width = 128;
+const characterMaps = {
+	'0' : [0, 1, 2, 3, 4, width, width+3, width+4, (width*2), (width*2)+2, (width*2)+4, (width*3), (width*3)+1, (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4], 
+	'1': [],
+	// '2': [], 
+	// '3': [], 
+	// '4': [], 
+	// '5': [],
+	// '6': [],      
+	// '7': [],   
+	'8': [0, 1, 2, 3, 4, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],   
+	'9': [0, 1, 2, 3, 4, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3)+4, (width*4)+4],   	   
+	' ': [],	
+	A : [2, width+1, width+3, width*2, (width*2)+4, width*3, (width*3)+1, (width*3)+2, (width*3)+3,(width*3)+4, (width*4), (width*4)+4],
+	B : [0,1,2,3, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3), (width*3)+4, width*4, (width*4)+1, (width*4)+2, (width*4)+3],
+	C : [0, 1, 2, 3, 4, width, (width*2), (width*3), (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
+	D : [0, 1, 2, 3, width, width+4, (width*2), (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4)+1, (width*4)+2, (width*4)+3],
+	E : [0, 1, 2, 3, 4, width, (width*2), (width*2), (width*2)+1, (width*2)+2, (width*3), (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
+	F : [0, 1, 2, 3, 4, width, (width*2), (width*2), (width*2)+1, (width*2)+2, (width*3), (width*4), (width*4)],
+	G : [0, 1, 2, 3, 4, width, (width*2), (width*2)+2, (width*2)+3, (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
+	H : [0, 4, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4),(width*4)+4],
+	I : [0, 1, 2, 3, 4, width+2, (width*2)+2, (width*3)+2, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
+	J : [width+4, width*3, (width*2)+4, (width*3)+4, (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
+	K : [0, 4, width, width+3, (width*2), (width*2)+1, (width*2)+2, (width*3), (width*3)+3, (width*4), (width*4),(width*4)+4],
+	L : [0, width, (width*2), (width*3), (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
+	M : [0, 4, width, width+1, width+3, width+4, (width*2), (width*2)+2, (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4),(width*4)+4],
+	N : [0, 4, width, width+1, width+4, (width*2), (width*2)+2, (width*2)+4, (width*3), (width*3)+3, (width*3)+4, (width*4), (width*4),(width*4)+4],
+	O : [0, 1, 2, 3, 4, width, width+4, (width*2), (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
+	P : [0,1,2,3, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3), width*4],
+	Q : [0, 1, 2, 3, 4, width, width+4, (width*2), (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4, (width*5)+2, (width*3)+2],
+	R : [0,1,2,3, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, width*3, (width*3)+3, width*4, (width*4)+4],
+	S : [0, 1, 2, 3, 4, width, (width*2), (width*2)+4, (width*2)+1, (width*2)+2, (width*2)+3, (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
+	T : [0, 1, 2, 3, 4, width+2, (width*2)+2, (width*3)+2, (width*4)+2],
+	U : [0, 4, width, width+4, (width*2), (width*2)+4, (width*3), (width*3)+4, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
+	V : [0, 4, width, width+4, (width*2), (width*2)+4, (width*3)+1, (width*3)+3, (width*4)+2],
+	W : [0, 4, width, width+4, (width*2), (width*2)+4, width*3, (width*3)+1, (width*3)+3, (width*3)+4, (width*2)+2, width*4, (width*4)+4],
+	X : [0, 4, width+1, width+3, (width*2)+2, (width*3)+1, (width*3)+3, (width*2)+2, width*4, (width*4)+4],
+	Y : [0, 4, width, width+4, (width*2), (width*2)+1, (width*2)+2, (width*2)+3, (width*2)+4, (width*3)+2, (width*4)+2],
+	Z : [0, 1, 2, 3, 4, width+3, (width*2)+2, (width*3)+1, (width*4), (width*4), (width*4)+1, (width*4)+2, (width*4)+3, (width*4)+4],
+}
+
+function sayHello(divDisplay, inputText) {
+	let position = 129;
+	let char;
+	for (let i=0; i < inputText.length; i++) {
+		char =`${inputText[i]}`;		
+		if (characterMaps.hasOwnProperty(char)) {
+			writeChar(characterMaps[char], divDisplay, position);	
+		} else {
+			writeChar(characterMaps[" "], divDisplay, position);	
+		}
+		
+		if (position % 128 === 121) {
+			position+= 648;
+		} else {
+			position+=6;	
+		}
+	}
+}
+
+function writeChar(charDefinitionArr, divDisplay, position) {	
+	charDefinitionArr.forEach(charPixel => divDisplay[charPixel + position].classList.add('textPixel'))
 }
 
 function destroyDisplay() {
@@ -188,16 +254,17 @@ function displayDraw1(divDisplay, destroyedImg) {
 		if (monoValue % (rand1 * 5) === 0
 			|| monoValue % (rand1 * 3) === 0 ) modR = rndNum(100);
 		if (monoValue % (rand2 * 3) === 0 ) modG = -rndNum(100);
-		if (monoValue < 30) monoValue -= 20;
+		if (monoValue < 20) monoValue -= 20;
+		if (monoValue < 200) monoValue -= 20;
 		div.style=`background-color: rgba(
-		${monoValue + modR - modG},
-		${monoValue - (modR/2)},
-		${monoValue + modG},
+		${monoValue - modR - modG},
+		${monoValue + (modR/2)},
+		${monoValue - modG},
 		1);`
 		modR-=5;
 		modG+=15;
 		if (modR <= -55 || modR >= 150) modR = 0;
-		if (modG >= 100 || modG <= -100) modG = 0;
+		if (modG >= 100 || modG <= -100) modG = 0;		
 		
 	});
 }
